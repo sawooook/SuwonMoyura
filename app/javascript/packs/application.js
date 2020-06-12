d0ec98c57b26a4ebd0a09919251bf877ee64a932
saouk <honggalra2@gmail.com>
2020-06-12 21:43:41 +0900

    add stimuls js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,23 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+import './src/application'// Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+const application = Application.start();
+const context = require.context("controllers", true, /.js$/);
+application.load(definitionsFromContext(context));
