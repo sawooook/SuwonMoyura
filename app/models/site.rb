fd980267144af488649fea072c87be573cf07158
saouk <honggalra2@gmail.com>
2020-06-26 22:14:37 +0900

    add method name and add nav bar

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,15 +1,16 @@
 class Site < ApplicationRecord
-
+  #- 위치가 없는 장소들은 제외
   def self.list_of_site
     where.not(lat: nil, lng: nil)
   end
 
+  #- Redis에 장소들의 데이터를 저장한다.
   def self.create_site_json
     MyRedis.set("site_json_cache", ResponseWrapper.wrap_as_list(Site.list_of_site))
   end
 
-
-  def self.cached_site
+  #- Redis에 저장한 데이터를 불러옴
+  def self.load_cached_site
     MyRedis.getData("site_json_cache")
   end
 end
