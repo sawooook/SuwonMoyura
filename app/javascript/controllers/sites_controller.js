8b94a6b87f89a754dbf3a218d290b0cf559b2781
saouk <honggalra2@gmail.com>
2020-07-02 19:47:36 +0900

    스피너 화면 추가 ,  화면상에서 보일때 불필요한 마커 제거,  메인컬러 추가

--- app/javascript/controllers/sites_controller.js
+++ app/javascript/controllers/sites_controller.js
@@ -2,12 +2,19 @@ import {Controller} from "stimulus"
 
 export default class extends Controller{
     static get targets() {
-        return [ "map", "modal"]
+        return [ "map", "modal", "title", "address"]
     }
 
     connect() {
+        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
+            imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+            imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
         var markers = [];
         this.loadMyLocation()
+        let controller = this
+
+        $('#progressBar').fadeOut()
 
         //- view에 카카오 지도를 뿌림
         var Cheight = $(window).height();
@@ -17,7 +24,7 @@ export default class extends Controller{
         var options = {
             center: new kakao.maps.LatLng(37.2788, 126.953),
             level: 2,
-            maxLevel: 4
+            maxLevel: 3
         };
 
         //- 맵성 객체 생성
@@ -35,6 +42,10 @@ export default class extends Controller{
             var latlng = map.getCenter();
             var x = latlng.getLat()
             var y = latlng.getLng()
+            var Cheight = $(window).height();
+            $('#progressBar').css({'margin-top':Cheight/2.5+'px'});
+            $('#progressBar').fadeIn()
+
 
             //- 1. ajax를 이용하여 x,y축 좌표값을 보낸다.
             //- 2. rails에서 위치값을 계산 한 후 response값으로 리턴
@@ -43,27 +54,42 @@ export default class extends Controller{
                 type : "GET",
                 dataType: "json",
                 success: function(args) {
-                    console.log(args)
-                    var parseSite  = args.data
+                    console.log(markers)
 
+                    for (var index = 0; index < markers.length; index++){
+                        markers[index].setMap(null)
+                    }
+
+                    var parseSite  = args.data
+                    clusterer.clear()
                     //- 받아온 데이터를 for문을 이용하여 마커를 추가해준다
                     parseSite.forEach(function (k) {
                         var markerPosition  = new kakao.maps.LatLng(k["lat"], k["lng"]);
+                        var markerTitle = k["name"]
+                        var marketAddress = k["address"]
+
+                        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+
                         var marker = new kakao.maps.Marker({
-                            position: markerPosition
+                            position: markerPosition,
+                            image: markerImage // 마커이미지 설정
                         });
+
+
+
                         marker.setMap(map);
 
-                        var iwContent = `<div style="padding:5px;">${k["name"]}</div>`
-                        var infowindow = new kakao.maps.InfoWindow({
-                            content : iwContent
-                        });
+
                         kakao.maps.event.addListener(marker, 'click', function (e) {
+                            controller.titleTarget.innerHTML = markerTitle
+                            controller.addressTarget.innerHTML = marketAddress
                             $('#myModal').modal('show');
                         })
+
                         markers.push(marker)
+                        $('#progressBar').fadeOut()
+
                     })
-                    clusterer.addMarkers(markers)
                 },
                 error : function(xhr, status, error){
                     //요청에 실패하면 에러코드 출력
@@ -84,5 +110,11 @@ export default class extends Controller{
             maximumAge: 0,
             timeout: Infinity
         });
+    }
+
+    clearMarker(markers){
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
     }
 }
